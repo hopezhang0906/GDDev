4f216c6e5d69b2a0833c4ca079eb211a283aad6d
John Cido <johncido@outlook.com>
2017-05-01 17:18:10 +0800

    server.js quick optimize

--- js/server/server.js
+++ js/server/server.js
@@ -1,23 +1,28 @@
 'use strict';
 
-var express = require('express');
-var app = express();
 var path = require('path');
+var express = require('express');
 
+var app = express();
 app.use('/public', express.static(path.resolve(__dirname, '../dist/public')));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../dist/index.html'))
 })
 
-app.get('/login', function (req, res) {
+app.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../dist/login.html'))
 })
 
-app.get('/creat', function (req, res) {
+app.get('/creat', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../dist/createnewone.html'))
 })
 
-app.listen(8860, function () {
+app.get('/database/project/:id', (req, res) => {
+    console.log('project request')
+    res.json({ id: req.params.id })
+})
+
+app.listen(8860, () => {
     console.log("Go!");
 })
